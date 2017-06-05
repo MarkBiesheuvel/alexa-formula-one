03e3221d2492eef8ab119a44b875350a5416b141
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-06-05 08:37:30 +0200

    Store standings in DynamoDB

--- process/index.js
+++ process/index.js
@@ -1,10 +1,11 @@
 const cheerio = require('cheerio')
+const AWS = require('aws-sdk')
 
 const year = process.argv[2] || 2017
 
 const response = require(`../ingest/standings_${year}.json`)
 
-const standings = []
+const items = []
 
 const $ = cheerio.load(response.parse.text['*'])
 const table = $('#Results_and_standings').parent().nextAll('table').eq(2)
@@ -17,9 +18,32 @@ while(tr.length > 0) {
     const name = tr.find('td').eq(0).find('a').eq(1).text()
     const points = tr.find('th').eq(1).text()
 
-    standings.push({ position, name, points })
+    items.push({
+      PutRequest: {
+        Item: {
+          Year: parseInt(year),
+          Position: parseInt(position),
+          DriverName: name,
+          Points: parseInt(points)
+        }
+      }
+    })
   }
   tr = tr.next()
 }
 
-console.log(standings)
+const client = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-1' })
+const batchSize = 25
+for (let i = 0; i < items.length; i+= batchSize) {
+  client.batchWrite({
+    RequestItems: {
+      FormulaOneStandings: items.slice(i, i + batchSize)
+    }
+  }, (error, response) => {
+    if (error) {
+      console.error(error)
+    } else {
+      console.log(response)
+    }
+  })
+}
