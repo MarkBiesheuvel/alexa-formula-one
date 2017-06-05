8a64c16db28117eb7b39080f1d5c4ec4656c9f6c
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-06-05 09:29:23 +0200

    Fix bug with missing tables

--- process/index.js
+++ process/index.js
@@ -8,8 +8,8 @@ const response = require(`../ingest/standings_${year}.json`)
 const items = []
 
 const $ = cheerio.load(response.parse.text['*'])
-const table = $('#Results_and_standings').parent().nextAll('table').eq(2).find('.wikitable').eq(0)
-let tr = table.find('tr').eq(1)
+const table = $('#Results_and_standings').parent().nextAll('table').filter((i, el) => ($(el).find('th').eq(1).text() === 'Driver')).eq(0)
+let tr = table.find('.wikitable').eq(0).find('tr').eq(1)
 
 while(tr.length > 0) {
   const position = tr.find('th,td').eq(0).text()
